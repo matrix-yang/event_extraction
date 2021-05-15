dd1a7f1eadedf27af029c514f5e89523ba285cb6
Arnold <739260804@qq.com>
2021-05-15 12:49:06 +0800

    crf + joint bert

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -114,6 +114,83 @@ class DuEEClsDataset(Dataset):
         return self.tokenized_examples[index]
 
 
+class DuEEJointDataset(Dataset):
+    """DuEventExtraction"""
+
+    def __init__(self, args, tagger_data_path, role_data_path, tagger_tag_path, role_tag_path, tokenizer):
+        self.tagger_dataset = DuEEEventDataset(args,
+                                               tagger_data_path,
+                                               tagger_tag_path,
+                                               tokenizer)
+        self.role_dataset = DuEEEventDataset(args,
+                                             role_data_path,
+                                             role_tag_path,
+                                             tokenizer)
+
+        self.algt_tagger = []
+        self.algt_role = []
+
+        self.alignment()
+
+        assert len(self.algt_tagger) == len(self.algt_role), '数据长度不一致'
+
+    def alignment(self):
+        tag_idx = 0
+        role_idx = 0
+        while tag_idx < len(self.tagger_dataset) and role_idx < len(self.role_dataset):
+            tag_token_ids = self.tagger_dataset[tag_idx]['input_ids']
+            role_token_idx = self.role_dataset[role_idx]['input_ids']
+
+            if tag_token_ids == role_token_idx:
+                self.algt_tagger.append(self.tagger_dataset[tag_idx])
+                self.algt_role.append(self.role_dataset[role_idx])
+
+                assert self.tagger_dataset[tag_idx]['attention_mask'] == self.role_dataset[role_idx][
+                    'attention_mask'], 'atten_mask 不一致'
+
+                tag_idx += 1
+                role_idx += 1
+            else:
+                tag_idx -= 1
+
+        return len(self.role_dataset)
+
+    def __len__(self):
+        return len(self.algt_role)
+
+    def __getitem__(self, index):
+        tragger = self.algt_tagger[index]
+        role = self.algt_role[index]
+
+        joint_data = {}
+        joint_data['input_ids'] = tragger['input_ids']
+        joint_data['token_type_ids'] = tragger['token_type_ids']
+        joint_data['attention_mask'] = tragger['attention_mask']
+        joint_data['tagger_labels'] = tragger['labels']
+        joint_data['role_labels'] = role['labels']
+
+        return joint_data
+
+
+def joint_collate_fn(batch):
+    max_len = max([sum(x['attention_mask']) for x in batch])
+    seq_lens = torch.tensor([sum(x["attention_mask"]) for x in batch])
+    all_input_ids = torch.tensor([x['input_ids'][:max_len] for x in batch])
+    all_token_type_ids = torch.tensor([x['token_type_ids'][:max_len] for x in batch])
+    all_attention_mask = torch.tensor([x['attention_mask'][:max_len] for x in batch])
+    all_tagger_labels = torch.tensor([x["tagger_labels"][:max_len] for x in batch])
+    all_role_labels = torch.tensor([x["role_labels"][:max_len] for x in batch])
+
+    return {
+        "all_input_ids": all_input_ids,
+        "all_token_type_ids": all_token_type_ids,
+        "all_attention_mask": all_attention_mask,
+        "all_tagger_labels": all_tagger_labels,
+        "all_role_labels": all_role_labels,
+        "all_seq_lens": seq_lens
+    }
+
+
 def collate_fn(batch):
     max_len = max([sum(x['attention_mask']) for x in batch])
     seq_lens = torch.tensor([sum(x["attention_mask"]) for x in batch])
